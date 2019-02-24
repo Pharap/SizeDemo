1693d99bc083fd1d03ea2bcf4b56b44e75751a7e
Pharap <2933055+Pharap@users.noreply.github.com>
2019-02-24 20:18:40 +0000

    Add code files

---
+++ SizeDemo.ino
@@ -0,0 +1,33 @@
+#include <Arduboy2.h>
+
+#include "Size.h"
+
+Arduboy2 arduboy;
+
+const char exampleA[8] PROGMEM {};
+const int exampleB[4] PROGMEM {};
+const uint8_t exampleC[10] PROGMEM {};
+
+void setup()
+{
+	arduboy.begin();
+}
+
+void loop()
+{
+	if(!arduboy.nextFrame())
+		return;
+	
+	arduboy.clear();
+	
+	arduboy.print(F("exampleA: "));
+	arduboy.println(getSize(exampleA));
+	
+	arduboy.print(F("exampleB: "));
+	arduboy.println(getSize(exampleB));
+	
+	arduboy.print(F("exampleC: "));
+	arduboy.println(getSize(exampleC));
+	
+	arduboy.display();
+}
